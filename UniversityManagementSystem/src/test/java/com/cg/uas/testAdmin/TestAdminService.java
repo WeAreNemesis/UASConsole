350badbd25c890655ae48263488467c7cbb61e1e
code-picasso <mail.shailesh.singh@gmail.com>
2020-04-08 12:34:39 +0530

    second commit

---
+++ UniversityManagementSystem/src/test/java/com/cg/uas/testAdmin/TestAdminService.java
@@ -0,0 +1,24 @@
+package com.cg.uas.testAdmin;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class TestAdminService {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
